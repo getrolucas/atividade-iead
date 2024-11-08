4e4a55e776f035f2517fe30efe33dc15860ff2d6
Getro Lucas <142281291+getrolucas@users.noreply.github.com>
2024-11-07 23:11:42 -0300

    First commit

---
+++ Aprendizagem.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <div style="text-align: justify;">
+    
+    ## Computação: Um Panorama Histórico
+    
+    O objeto de aprendizagem proposto é um software de aprendizagem voltado para o ensino da História da Computação 
+    para alunos do Ensino Fundamental e Médio. 
+    O software apresenta um texto base sobre os marcos históricos da computação, complementado por 
+    uma vídeo-aula e, em seguida, propõe um questionário para reforçar a aprendizagem do aluno. 
+    O objetivo é facilitar o aprendizado de conceitos históricos e tecnológicos de forma multimodal.
+
+    
+    #### Área proposta para desenvolvimento de OA:
+    História da Computação
+
+    #### Nível de ensino do conteúdo proposto para o OA:
+    Ensino Fundamental e Médio
+
+    #### Conteúdo proposto para o desenvolvimento de OA:
+    O aplicativo aborda o tema História da Computação, oferecendo um conteúdo multimodal que 
+    inclui texto, vídeo-aula e um Questionário.
+
+    #### Identificação do tipo de OA:
+    Software educativo, Texto, Vídeo aula e questionário. 
+    
+    #### Público-alvo (alunos, professores):
+    Alunos
+    
+    </div>
+    """,
+    unsafe_allow_html=True
+)
